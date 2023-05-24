fd358d8e8962165c43b2ea1381352ed569fe7860
bekzodxudaybergenow <bekzodxudaybergenow@gmail.com>
2023-05-24 10:43:29 +0500

    Add Faq Section

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// let faqBtn = document.querySelectorAll('.faq-question-btn'),
+//     faqBlogText = document.querySelector('.faq-blog-text'),
+//     faqQuestionBtnIcon = document.querySelector('.faq-question-btn-icon');
+
+
+// const faqClick = (event) => {
+//     faqBlogText.classList.toggle('block');
+//     console.log(event.target);
+// }
+
+
+
+// faqBtn.forEach(item => {
+//     item.addEventListener('click', faqClick);
+// })
